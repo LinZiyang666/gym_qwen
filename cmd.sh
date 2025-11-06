a1e2a0f2793697d832a722e70e2ac95b308603fa
LinZiyang666 <3177267975@qq.com>
2025-11-06 16:29:35 -0600

    撒地方

---
+++ cmd.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+torchrun --nproc_per_node=8 \
+  scripts/train_pipeline_ddp.py \
+  --model qwen3 \
+  --env halfcheetah \
+  --dataset medium \
+  --pipeline_stages 4 \
+  --data_parallel_groups 2 \
+  --device_groups '0,1,2,3;4,5,6,7' \
+  --batch_size 32 \
+  --micro_batches 4 \
+  --n_blocks 24 \
+  --embed_dim 1024 \
+  --n_heads 16 \
+  --num_kv_heads 8 \
+  --head_dim 64 \
+  --lr 1e-4 \
+  --warmup_steps 10000 \
+  --grad_clip 0.25 \
+  --log_dir dt_runs/qwen3_pp "$@"
