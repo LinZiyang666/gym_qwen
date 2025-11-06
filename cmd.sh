9bbb80b9d7330170a64a1ba4eb7ada9484573fbb
LinZiyang666 <3177267975@qq.com>
2025-11-06 16:38:56 -0600

    cmd

--- cmd.sh
+++ cmd.sh
@@ -2,9 +2,8 @@
 
 set -euo pipefail
 
-export NCCL_SOCKET_IFNAME=lo
+export NCCL_SOCKET_IFNAME=enp129s0f1
 export NCCL_IB_DISABLE=1
-export NCCL_P2P_DISABLE=0
 export NCCL_DEBUG=INFO
 export TORCH_NCCL_ASYNC_ERROR_HANDLING=1
 
